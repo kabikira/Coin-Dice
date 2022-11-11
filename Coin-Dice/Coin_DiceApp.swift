164f5e9285647aa9f5562c02c70f77d0ad6b9519
kabikira <kabikirar2@protonmail.com>
2022-11-11 11:58:48 +0900

    Initial Commit

---
+++ Coin-Dice/Coin_DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coin_DiceApp.swift
+//  Coin-Dice
+//
+//  Created by koala panda on 2022/11/11.
+//
+
+import SwiftUI
+
+@main
+struct Coin_DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
